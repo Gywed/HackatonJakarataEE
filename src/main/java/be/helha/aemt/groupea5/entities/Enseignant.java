451c0c9d158b96f92703f9aa65e36006b2d7f62c
Yohan <yohan.noel.huls@gmail.com>
2023-01-16 11:26:24 +0100

    Complet Enseignant.java

--- src/main/java/be/helha/aemt/groupea5/entities/Enseignant.java
+++ src/main/java/be/helha/aemt/groupea5/entities/Enseignant.java
@@ -1,5 +1,35 @@
 package be.helha.aemt.groupea5.entities;
 
-public class Enseignant {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Enseignant implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nom;
+	private String prenom;
+	private String mail;
+	private String remarque;
+	private Attribution attribution;
+	
+	public Enseignant() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Enseignant(String nom, String prenom, String mail, String remarque, Attribution attribution) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.mail = mail;
+		this.remarque = remarque;
+		this.attribution = attribution;
+	}
 }
