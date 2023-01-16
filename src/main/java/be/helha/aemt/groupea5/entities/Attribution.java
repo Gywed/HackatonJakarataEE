b121a4c9c636b9c65f666a6ce2a015f69084b3ea
Yohan <yohan.noel.huls@gmail.com>
2023-01-16 11:36:54 +0100

    Create and complet Attribution.java

---
+++ src/main/java/be/helha/aemt/groupea5/entities/Attribution.java
@@ -0,0 +1,31 @@
+package be.helha.aemt.groupea5.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Attribution {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String anneeAcademique;
+	private List<AA> aas;
+	private List<Mission> missions;
+	
+	public Attribution() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Attribution(String anneeAcademique, List<AA> aas, List<Mission> missions) {
+		super();
+		this.anneeAcademique = anneeAcademique;
+		this.aas = aas;
+		this.missions = missions;
+	}
+}
