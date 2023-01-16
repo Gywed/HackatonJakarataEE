6dfa9084d175f18f86a5ade3ca5e5a877e7bb2d5
adujacqu <alex.dujacquier@gmail.com>
2023-01-16 12:50:11 +0100

    add findAll, find, add, delete and update to EnseignantDAO

--- src/main/java/be/helha/aemt/groupea5/dao/EnseignantDAO.java
+++ src/main/java/be/helha/aemt/groupea5/dao/EnseignantDAO.java
@@ -1,15 +1,72 @@
 package be.helha.aemt.groupea5.dao;
 
+import java.util.List;
+
+import be.helha.aemt.groupea5.entities.Enseignant;
 import jakarta.ejb.LocalBean;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 @Stateless
 @LocalBean
 public class EnseignantDAO {
 	
 	@PersistenceContext(unitName = "groupeA5-JTA")
-	private EntityManager em; 
+	private EntityManager em;
+
+	public EnseignantDAO() {
+		super();
+		// TODO Auto-generated constructor stub
+	} 
+	
+	public List<Enseignant> findAll(){
+		String strUA="Select e from Enseignant e ";
+		TypedQuery<Enseignant> queryA = em.createQuery(strUA, Enseignant.class);
+		return queryA.getResultList();
+	}
+	
+	public Enseignant find(Enseignant e) {
+		TypedQuery<Enseignant> query = em.createQuery("Select e from Enseignant e where e.email = ?1", Enseignant.class);
+		query.setParameter(1, e.getMail());
+		List<Enseignant> result = query.getResultList();
+		System.out.println(result);
+		return result.isEmpty() ? null : result.get(0);
+	}
+	
+	public Enseignant add(Enseignant e) {
+		if (e==null) {
+			return null;
+		}
+		
+		if (find(e) != null) {
+			return null;
+		}
+		return em.merge(e);
+
+		
+	}
+	
+	public Enseignant delete(Enseignant e) {
+		if (e==null) {
+			return null;
+		}
+		
+		em.remove(find(e));
+		
+		return e;
+	}
+	
+	public Enseignant update(Enseignant e) {
+		if (e==null) return null;
+		
+		Enseignant dbE = find(e);
+		if(dbE==null) return null;
+		e.setId(dbE.getId());
+		
+		return em.merge(e);
+	
+	}
 
 }
