4015a3b85bd5b8a76985e8f6dee09f1e61bb629b
Yohan <yohan.noel.huls@gmail.com>
2023-01-16 12:49:26 +0100

    Create RestService

---
+++ src/main/java/be/helha/aemt/groupea5/config/RestService.java
@@ -0,0 +1,9 @@
+package be.helha.aemt.groupea5.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/groupeA5")
+public class RestService extends Application{
+
+}
