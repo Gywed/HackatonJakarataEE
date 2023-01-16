fcde08c43e57efd3a133cd0b381da477b4499c40
Yohan <yohan.noel.huls@gmail.com>
2023-01-16 15:12:54 +0100

    Remove any occurence of rest API

--- src/main/java/be/helha/aemt/groupea5/config/RestService.java
+++
@@ -1,9 +0,0 @@
-package be.helha.aemt.groupea5.config;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/groupeA5")
-public class RestService extends Application{
-
-}
