c8b91c96dcf548ec5f29a316c032480e1f0af091
Yohan <yohan.noel.huls@gmail.com>
2023-01-17 12:36:37 +0100

    Add list AA and add AA button

--- src/main/java/be/helha/aemt/groupea5/control/AAControl.java
+++ src/main/java/be/helha/aemt/groupea5/control/AAControl.java
@@ -7,6 +7,7 @@ import org.jboss.weld.context.ejb.Ejb;
 
 import be.helha.aemt.groupea5.ejb.AAEJB;
 import be.helha.aemt.groupea5.entities.AA;
+import be.helha.aemt.groupea5.entities.Fraction;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
@@ -15,6 +16,17 @@ import jakarta.inject.Named;
 public class AAControl implements Serializable {
 	@Ejb
 	private AAEJB beanGestion;
+	
+	private Integer anneeAcademique;
+	private String code;
+	private String intitule;
+	private Integer credit;
+	private Integer heure;
+	private Integer heureQ1;
+	private Integer heureQ2;
+	private Integer nombreGroupe;
+	private Integer nombreEtudiant;
+	private Fraction fraction;
 	
 	public AAControl() {
 		// TODO Auto-generated constructor stub
@@ -26,7 +38,97 @@ public class AAControl implements Serializable {
 	}
 			
 	public void doAddAa() {
-		beanGestion.add(new AA());
+		beanGestion.add(new AA(anneeAcademique,code,intitule,credit,heure,heureQ1,heureQ2,nombreGroupe,nombreEtudiant,fraction));
 	}
+
+	public AAEJB getBeanGestion() {
+		return beanGestion;
+	}
+
+	public void setBeanGestion(AAEJB beanGestion) {
+		this.beanGestion = beanGestion;
+	}
+
+	public Integer getAnneeAcademique() {
+		return anneeAcademique;
+	}
+
+	public void setAnneeAcademique(Integer anneeAcademique) {
+		this.anneeAcademique = anneeAcademique;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getIntitule() {
+		return intitule;
+	}
+
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+
+	public Integer getCredit() {
+		return credit;
+	}
+
+	public void setCredit(Integer credit) {
+		this.credit = credit;
+	}
+
+	public Integer getHeure() {
+		return heure;
+	}
+
+	public void setHeure(Integer heure) {
+		this.heure = heure;
+	}
+
+	public Integer getHeureQ1() {
+		return heureQ1;
+	}
+
+	public void setHeureQ1(Integer heureQ1) {
+		this.heureQ1 = heureQ1;
+	}
+
+	public Integer getHeureQ2() {
+		return heureQ2;
+	}
+
+	public void setHeureQ2(Integer heureQ2) {
+		this.heureQ2 = heureQ2;
+	}
+
+	public Integer getNombreGroupe() {
+		return nombreGroupe;
+	}
+
+	public void setNombreGroupe(Integer nombreGroupe) {
+		this.nombreGroupe = nombreGroupe;
+	}
+
+	public Integer getNombreEtudiant() {
+		return nombreEtudiant;
+	}
+
+	public void setNombreEtudiant(Integer nombreEtudiant) {
+		this.nombreEtudiant = nombreEtudiant;
+	}
+
+	public Fraction getFraction() {
+		return fraction;
+	}
+
+	public void setFraction(Fraction fraction) {
+		this.fraction = fraction;
+	}
+	
+	
 
 }
