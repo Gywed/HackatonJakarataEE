67d58731e04de080b8189332585b6b7e83b1c121
adujacqu <alex.dujacquier@gmail.com>
2023-01-17 12:47:55 +0100

    create AnneeAcadémiqueEJB

--- src/main/java/be/helha/aemt/groupea5/ejb/AnneeAcademiqueEJB.java
+++ src/main/java/be/helha/aemt/groupea5/ejb/AnneeAcademiqueEJB.java
@@ -1,10 +1,13 @@
 package be.helha.aemt.groupea5.ejb;
 
+import be.helha.aemt.groupea5.dao.AnneeAcademiqueDAO;
+import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 
 @Stateless
 public class AnneeAcademiqueEJB {
 	
-	
+	@EJB
+	private AnneeAcademiqueDAO dao;
 
 }
