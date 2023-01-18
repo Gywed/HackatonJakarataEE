757bf1f23b44dc8bf4e3049607cfff827aae5937
Adamaheto Saviour <skaytroz@gmail.com>
2023-01-18 12:14:58 +0100

    add sectionEJB

---
+++ src/main/java/be/helha/aemt/groupea5/ejb/SectionEJB.java
@@ -0,0 +1,40 @@
+package be.helha.aemt.groupea5.ejb;
+
+import java.util.List;
+
+import be.helha.aemt.groupea5.dao.SectionDAO;
+import be.helha.aemt.groupea5.entities.Section;
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class SectionEJB 
+{
+	@EJB
+	private SectionDAO dao;
+	
+	public Section find(Section e) 
+	{
+		return dao.find(e);
+	}
+
+	public List<Section> findAll() 
+	{
+		return dao.findAll();
+	}
+
+	public Section add(Section e) 
+	{
+		return dao.add(e);
+	}
+
+	public Section delete(Section e) 
+	{
+		return dao.delete(e);
+	}
+
+	public Section update(Section e) 
+	{
+		return dao.update(e);
+	}
+}
