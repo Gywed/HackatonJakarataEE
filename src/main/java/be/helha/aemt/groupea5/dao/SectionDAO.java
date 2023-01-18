74bb349cc1bea069b8a401b4d24a5fa479cafe3a
Adamaheto Saviour <skaytroz@gmail.com>
2023-01-18 12:15:34 +0100

    add sectionDao

---
+++ src/main/java/be/helha/aemt/groupea5/dao/SectionDAO.java
@@ -0,0 +1,73 @@
+package be.helha.aemt.groupea5.dao;
+
+import java.util.List;
+import be.helha.aemt.groupea5.entities.Section;
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+
+@Stateless
+@LocalBean
+public class SectionDAO 
+{
+	@PersistenceContext(unitName = "groupeA5-JTA")
+	private EntityManager em;
+
+	
+	public SectionDAO() 
+	{
+		super();
+	}
+	
+	public Section find(Section section) 
+	{
+		TypedQuery<Section> query = em.createNamedQuery("findByName",Section.class);
+		query.setParameter(1, section.getNom());
+		List<Section> list = query.getResultList();
+		
+		em.clear();
+		
+		return list.isEmpty() ? null : list.get(0);
+	}
+	
+	public List<Section> findAll() 
+	{
+		String strQuery="Select s from Section s";
+		TypedQuery<Section> query = em.createQuery(strQuery,Section.class);
+		return query.getResultList();
+	}
+	
+	public Section add(Section e) 
+	{
+		if (e == null) return null;
+		
+		if (e == find(e)) return null;
+		
+		return em.merge(e);
+	}
+	
+	public Section delete(Section e) 
+	{
+		if (e == null) return null;
+		
+		Section dbE = find(e);
+		if (dbE == null) return null;
+		
+		em.remove(em.merge(dbE));
+		
+		return dbE;
+	}
+
+	public Section update(Section e) 
+	{
+		if (e == null) return null;
+		
+		Section dbE = find(e);
+		
+		e.setId(dbE.getId());
+		
+		return em.merge(e);
+	}
+}
