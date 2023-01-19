7628aade424cbe24faf36e74526a8769c9b6fbc9
Yohan <yohan.noel.huls@gmail.com>
2023-01-19 21:30:36 +0100

    Create WrongArgumentException

---
+++ src/main/java/be/helha/aemt/groupea5/exception/WrongArgumentException.java
@@ -0,0 +1,9 @@
+package be.helha.aemt.groupea5.exception;
+
+public class WrongArgumentException extends Exception{
+
+	public WrongArgumentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
