3b0a543c15e00cc8e223cb56724736037e0aae99
Yohan <yohan.noel.huls@gmail.com>
2023-01-19 10:01:39 +0100

    Create Utilisateur entity

--- src/main/java/be/helha/aemt/groupea5/entities/Utilisateur.java
+++ src/main/java/be/helha/aemt/groupea5/entities/Utilisateur.java
@@ -7,11 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
-import jakarta.persistence.OneToMany;
 
 @Entity
 @NamedQueries({
