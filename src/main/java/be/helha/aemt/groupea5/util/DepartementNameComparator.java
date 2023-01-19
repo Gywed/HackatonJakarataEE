b47cd6553c7ad20682e398174b07d7edc720f525
Adamaheto Saviour <skaytroz@gmail.com>
2023-01-19 12:31:48 +0100

    add department name comparator

---
+++ src/main/java/be/helha/aemt/groupea5/util/DepartementNameComparator.java
@@ -0,0 +1,13 @@
+package be.helha.aemt.groupea5.util;
+
+import java.util.Comparator;
+
+import be.helha.aemt.groupea5.entities.Departement;
+
+public class DepartementNameComparator implements Comparator<Departement>
+{
+	  @Override
+	    public int compare(Departement d1, Departement d2) {
+	        return d1.getNom().compareTo(d2.getNom());
+	    }
+}
