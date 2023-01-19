fbc7f9471b4869de53f35fef4fb5ea958b29eebc
adujacqu <alex.dujacquier@gmail.com>
2023-01-19 13:00:51 +0100

    create AAConverter

---
+++ src/main/java/be/helha/aemt/groupea5/entities/AAConverter.java
@@ -0,0 +1,29 @@
+package be.helha.aemt.groupea5.entities;
+
+
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.FacesConverter;
+
+@FacesConverter(value = "converterAA")
+public class AAConverter implements Converter<AA>{
+	
+
+	@Override
+	  public AA getAsObject(FacesContext fc, UIComponent comp, String value) {
+		String[] splittedString = value.split("\\+");
+		AA a = new AA(null, splittedString[1], null , null, null, null, null, null, null, null);
+		a.setId(Integer.parseInt(splittedString[0]));
+		return a;
+	  }
+
+	  @Override
+	  public String getAsString(FacesContext fc, UIComponent comp, AA value) {
+	      return value.getId() +"+"+ value.getCode();
+	      
+	  }
+
+    
+
+}
