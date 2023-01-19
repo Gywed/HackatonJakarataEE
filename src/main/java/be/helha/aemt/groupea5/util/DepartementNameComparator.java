3ae0d94c363f613eaf8e3df6bec494d0c7b36507
Adamaheto Saviour <skaytroz@gmail.com>
2023-01-19 12:55:37 +0100

    add delete to departement

--- src/main/java/be/helha/aemt/groupea5/util/DepartementNameComparator.java
+++ src/main/java/be/helha/aemt/groupea5/util/DepartementNameComparator.java
@@ -8,6 +8,6 @@ public class DepartementNameComparator implements Comparator<Departement>
 {
 	  @Override
 	    public int compare(Departement d1, Departement d2) {
-	        return d1.getNom().compareTo(d2.getNom());
-	    }
+	       return d1.getNom().compareTo(d2.getNom());
+	   }
 }
